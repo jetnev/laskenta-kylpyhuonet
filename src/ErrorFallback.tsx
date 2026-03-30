2d7f79c8b61d66f502653e5c742bacd4750b8d96
Jethro Nevalainen <jethro.nevalainen@protonmail.com>
2026-03-30 18:46:17 +0000

    Edited Spark

--- src/ErrorFallback.tsx
+++ src/ErrorFallback.tsx
@@ -1,20 +1,20 @@
 import { Button } from "./components/ui/button";
 
-type Props = {
-  error: Error;
+  resetErrorBo
+
   resetErrorBoundary: () => void;
 };
 
 export default function ErrorFallback({ error, resetErrorBoundary }: Props) {
-  return (
-    <div className="flex min-h-screen items-center justify-center p-4">
-      <div className="w-full max-w-md space-y-4 text-center">
-        <h1 className="text-2xl font-semibold text-foreground">
-          Hm, something went wrong
-        </h1>
-        <p className="text-muted-foreground">{error.message}</p>
-        <Button onClick={resetErrorBoundary}>Lataa uudelleen</Button>
-      </div>
-    </div>
+      </di
   );
-}
+
+
+
+
+
+
+      </div>
+
+
+
