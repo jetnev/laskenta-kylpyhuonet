fc1cab4df9ab254d015b9a62c36be646e5e726f7
Jethro Nevalainen <jethro.nevalainen@protonmail.com>
2026-03-30 18:23:49 +0000

    Edited Spark

--- src/ErrorFallback.tsx
+++ src/ErrorFallback.tsx
@@ -1,5 +1,5 @@
 import { Alert, AlertTitle, AlertDescription } from "./components/ui/alert";
-import { Button } from "./components/ui/button";
+
 
 export const ErrorFallback = ({
   error,
@@ -7,21 +7,21 @@ export const ErrorFallback = ({
 }: {
   error: Error;
   resetErrorBoundary: () => void;
-}) => (
-  <div className="flex items-center justify-center min-h-screen p-4">
-    <Alert variant="destructive" className="max-w-lg">
-      <AlertTitle>Something went wrong</AlertTitle>
-      <AlertDescription>
-        <pre className="text-xs overflow-auto whitespace-pre-wrap mt-2">
-          {error.message}
+      <
+        <pre className="text-xs overflow-auto whitespace-pre-wrap mt-
         </pre>
-        <Button 
           onClick={resetErrorBoundary}
-          className="mt-4"
-          variant="outline"
-        >
+          variant="outli
           Try again
-        </Button>
+      </AlertDescription>
+        </pre>
+
+
+
+          variant="outline"
+
+          Try again
+
       </AlertDescription>
     </Alert>
   </div>
